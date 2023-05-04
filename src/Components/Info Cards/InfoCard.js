c5406b170ace390aadd0ef12e25e27af1ccad26c
gthornbuckle <gthornbuckle@gmail.com>
2023-05-04 23:53:53 +0100

    Changes styling for cards

--- src/Components/Info Cards/InfoCard.js
+++ src/Components/Info Cards/InfoCard.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function InfoCard() {
+
   return (
     <div className="info_card selection:bg-teal-200 selection:text-teal-800">
-        <div>
-            <img src="https://placehold.co/400x400" className="w-full"></img>
-        </div>
-        <h3 className="py-3 text-teal-300 text-3xl">Title</h3>
-        <div className="h-px w-[90%] bg-slate-300"></div>
-        <p className="px-4 py-2 text-slate-200 text-lg text-justify">Description - Ut et et voluptate officia sit exercitation quis. Lorem anim incididunt amet exercitation eiusmod nulla Lorem exercitation est nostrud anim cillum nulla.</p>
-        <div className="h-px w-[90%] bg-slate-300"></div>
-        <div className="py-3 flex flex-row flex-wrap items-center justify-center text-teal-300 text-md text-center">
-            <p className="px-1">Technology 1</p>
-            <p className="px-1">Technology 2</p>
-            <p className="px-1">Technology 3</p>
-            <p className="px-1">Technology 4</p>
-            <p className="px-1">Technology 5</p>
-        </div>
-        <div className="w-full pb-3 flex flex-row items-center justify-around text-slate-100 text-lg font-['Work_Sans']">
-            <button className="w-36 h-12 bg-yellow-500">view</button>
-            <button className="w-36 h-12 bg-yellow-500">github</button>
+        <div className="h-80 w-3/5 bg-no-repeat bg-left-top" style={{backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/portfolio-da254.appspot.com/o/Playlist%20Viewer.png?alt=media&token=2f5218d6-be9d-45aa-b989-7b0a5c3163c2")`}}></div>
+        <div className='px-4 flex flex-col items-start'>
+          <h3 className="py-3 text-yellow-400 text-4xl font-md">Title</h3>
+          <div className="py-3 flex flex-row flex-wrap items-center justify-center text-teal-300 text-lg text-center">
+              <p className="pr-1">Technology 1</p>
+          </div>
+          <div className='h-px w-full bg-slate-200'></div>
+          <p className="py-3 text-slate-200 text-lg text-justify">Description - Ut et et voluptate officia sit exercitation quis. Lorem anim incididunt amet exercitation eiusmod nulla Lorem exercitation est nostrud anim cillum nulla.</p>
+          <div className="absolute right-0 bottom-0 pb-3 text-slate-500 text-xl font-['Work_Sans']">
+              <motion.button className="w-36 h-12 bg-teal-300 drop-shadow-sm"
+                whileTap={{scale: 0.9, borderRadius: "2rem"}}
+                whileHover={{backgroundColor: "#facc15"}}
+              >view</motion.button>
+              <motion.button className="w-36 h-12 bg-teal-300 drop-shadow-sm"
+                whileTap={{scale: 0.9, borderRadius: "2rem"}}
+                whileHover={{backgroundColor: "#facc15"}}
+              >github</motion.button>
+          </div>
         </div>
     </div>
   );
--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import InfoCard from '../Info Cards/InfoCard';
+import { db } from '../../firebase.config'
+import { getDocs, collection } from 'firebase/firestore';
+
+const ref = collection(db, "mywork");
+
+const getWork = async () =>{
+  try{
+    const workData = await getDocs(ref);
+    return workData.docs.map(e => ({...e.data()}));
+    
+  }catch (err){
+    console.log(err);
+  }
+}
+
+const cvLinkContainer = {
+  hover: {
+    transition:{
+        staggerChildren: 0.1
+    }
+  }
+}
+
+const cvChild = {
+  initial:{
+    y: 0
+  },
+  hover:{
+    y: -12
+  }
+}
 
 function Portfolio() {
+  const [cvHover, setcvHover] = useState(false);
+
   return (
     <>
     <section className='h-[50rem] pt-24 bg-slate-700 flex flex-row items-center justify-center'>
@@ -21,14 +54,26 @@ function Portfolio() {
             transition={{ duration: 1 }}
           >
             <p className="pb-2 text-slate-300 text-2xl text-left">
-              I'm George, aspiring front-end developer and recent <a className="text-yellow-400 hover:underline" href='https://www.edx.org' target='_blank'>eDx</a> bootcamp graduate. 
+              I'm George, aspiring front-end developer and recent <a className="text-yellow-400 hover:underline" href='https://www.edx.org' target='_blank' rel="noreferrer">eDx</a> bootcamp graduate. 
               This page showcases some of the various projects I have worked on so far, both for my bootcamp and personal self-improvement.
             </p>
             <p className="text-slate-300 text-2xl text-left">
               Combining my current coding knowledge along with my eye for design, I am motivated to step into my first developer role.
             </p>
           </motion.div>
-          <a className="py-4 text-teal-300 text-4xl font-bold drop-shadow-md font-['Work_Sans']">view<span className="text-slate-300">My</span><span className='text-yellow-400'>CV</span></a>
+          <motion.div className="py-4 w-fit cursor-pointer"
+            onMouseOver={() => setcvHover(true)} 
+            onMouseOut={() => setcvHover(false)}
+            initial={false}
+            animate={cvHover ? "hover" : "initial"}
+            variants={cvLinkContainer}
+          >
+            <motion.a className="py-4 flex flex-row text-4xl font-bold drop-shadow-md font-['Work_Sans'] decoration-slate-300">
+              <motion.div className="text-teal-300" variants={cvChild}>view</motion.div>
+              <motion.div className="text-slate-300" variants={cvChild}>My</motion.div>
+              <motion.div className="text-yellow-400" variants={cvChild}>CV</motion.div>
+            </motion.a>
+          </motion.div>
         </div>
       </div>
     </section>
@@ -36,18 +81,11 @@ function Portfolio() {
       <div className='mx-10 container flex flex-col'>
         <h1 className="py-4 text-slate-700 text-6xl font-bold font-['Work_Sans']"><span className="text-teal-600">my</span>Work_</h1>
         <h2 className="text-slate-700 text-2xl font-medium">Personal Projects</h2>
-          <div className='py-4 flex flex-row flex-wrap items-center justify-around'>
-            <InfoCard/>
-            <InfoCard/>
+          <div className='py-4'>
             <InfoCard/>
           </div>
         <h2 className="text-slate-700 text-2xl font-medium">Bootcamp Projects</h2>
-        <div className='py-4 flex flex-row flex-wrap items-center justify-around'>
-          <InfoCard/>
-          <InfoCard/>
-          <InfoCard/>
-          <InfoCard/>
-          <InfoCard/>
+        <div className='py-4 grid place-content-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4'>
         </div>
       </div>
       
